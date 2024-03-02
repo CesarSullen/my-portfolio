115445da9f1b60c4f75621fd796befd0f7fbb453
CesarSullen <cesarsuarez2303@gmail.com>
2024-03-02 16:08:13 -0500

    Added fade-in transitions

--- public/js/functions.js
+++ public/js/functions.js
@@ -11,3 +11,21 @@ function playVideo() {
   let packVideo = document.getElementById("packVideo");
   packVideo.play();
 }
+
+/* Observer */
+const sections = document.querySelectorAll(".to-appear");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle("show", entry.isIntersecting);
+      if (entry.isIntersecting) observer.unobserve(entry.target);
+    });
+  },
+  {
+    threshold: 0.1,
+  }
+);
+sections.forEach((section) => {
+  observer.observe(section);
+});
